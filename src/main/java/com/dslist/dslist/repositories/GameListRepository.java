5f14e815246fe73dcfafeec98a8bffca62c7fe9b
Emanuel Viktor <emanuel.bol.cco@gmail.com>
2025-08-20 13:07:37 -0300

    Get lists

---
+++ src/main/java/com/dslist/dslist/repositories/GameListRepository.java
@@ -0,0 +1,11 @@
+package com.dslist.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dslist.dslist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+	
+	
+
+}
